f0f3ec2b6e5b13cd7d9e9e174a2a90b190b74713
Anton Kabanov <imcreativchik@gmail.com>
2023-12-10 10:27:11 +0500

    refactor imports

--- backend/recipe/filters.py
+++ backend/recipe/filters.py
@@ -1,6 +1,6 @@
 from django_filters.rest_framework import FilterSet, filters
 
-from recipes.models import Ingredient, Recipe, Tag
+from recipe.models import Ingredient, Recipe, Tag
 
 
 class SearchIngredientFilter(FilterSet):
