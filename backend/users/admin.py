ba66cb82c24046bc319a900e7014d03de3317ecf
Anton Kabanov <imcreativchik@gmail.com>
2023-12-14 22:12:44 +0500

    add admin zone

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import Follow, User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'password')
+    list_filter = ('email', 'username')
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'following')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Follow, FollowAdmin)
