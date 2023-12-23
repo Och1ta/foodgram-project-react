9527f5e18d1d7e8bde80c2cc833909fb9db667a8
Anton Kabanov <imcreativchik@gmail.com>
2023-12-23 22:02:33 +0500

    refactor

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,13 +2,16 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (
-    UserViewSet, IngredientViewSet, RecipeViewSet, TagViewSet
+    CustomUserViewSet, IngredientViewSet, RecipeViewSet,
+    SubscribeView, SubscriptionViewSet, TagViewSet
 )
 
 
+app_name = 'api'
+
 router = DefaultRouter()
 
-router.register('users', UserViewSet, basename='users')
+router.register('users', CustomUserViewSet, basename='users')
 router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('tags', TagViewSet, basename='tags')
 router.register(
@@ -17,6 +20,16 @@ router.register(
 
 
 urlpatterns = [
+    path(
+        'users/subscriptions/',
+        SubscriptionViewSet.as_view(),
+        name='subscriptions'
+    ),
+    path(
+        'users/<int:user_id>/subscribe/',
+        SubscribeView.as_view(),
+        name='subscribe'
+    ),
     path('', include(router.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
