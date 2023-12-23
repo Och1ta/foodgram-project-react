3f95bae3b64ded7b043b14a919c09c6d8d381659
Anton Kabanov <imcreativchik@gmail.com>
2023-12-23 22:08:28 +0500

    upd

--- backend/recipe/admin.py
+++ backend/recipe/admin.py
@@ -1,47 +1,57 @@
 from django.contrib import admin
-from django.contrib.admin import display
 
-from recipe.models import (
-    Ingredient, IngredientsRecipe, Recipe, ShoppingCart, Tag, Favorite
-)
+from .models import (Favorite, Ingredient, IngredientInRecipe, Recipe,
+                     ShoppingCart, Tag)
 
 
-class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('author', 'name', 'in_favorites_amount')
-    list_filter = ('author', 'name', 'tags')
-
-    @display(description='Кол-во добавлений в избранное.')
-    def in_favorites_amount(self, obj):
-        """Отображаем общее число добавлений этого рецепта в избранное"""
-
-        return obj.favourites.count()
+class IngredientInRecipeInline(admin.TabularInline):
+    model = IngredientInRecipe
 
 
-class IngredientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'measurement_unit')
-    list_filter = ['name']
-
-
-class IngredientsRecipeAdmin(admin.ModelAdmin):
-    list_display = ('recipe', 'ingredient', 'amount')
+class IngredientInRecipeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'recipe', 'ingredient', 'amount')
+    search_fields = ('recipe__name', 'ingredient__name')
 
 
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'color', 'slug')
-    list_filter = ['slug']
+    list_display = ('pk', 'name', 'color', 'slug')
+    list_editable = ('name', 'color', 'slug')
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
 
 
-class ShoppingCartAdmin(admin.ModelAdmin):
-    list_display = ('recipe', 'user')
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'author', 'count_favorite')
+    search_fields = ('author', 'name', 'tags')
+    list_filter = ('author', 'name', 'tags')
+    inlines = [IngredientInRecipeInline]
+    empty_value_display = '-пусто-'
+
+    def count_favorite(self, obj):
+        """Отображаем общее число добавлений этого рецепта в избранное"""
+        return obj.favorite_recipe.count()
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'measurement_unit')
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
 
 
 class FavoriteAdmin(admin.ModelAdmin):
-    list_display = ('recipe', 'user')
+    list_display = ('pk', 'user', 'recipe')
+    search_fields = ('user__username', 'recipe__name',)
 
 
-admin.site.register(Favorite, FavoriteAdmin)
-admin.site.register(Ingredient, IngredientAdmin)
-admin.site.register(Recipe, RecipeAdmin)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'recipe')
+    search_fields = ('user__username', 'recipe__name',)
+
+
+admin.site.register(IngredientInRecipe, IngredientInRecipeAdmin)
 admin.site.register(Tag, TagAdmin)
-admin.site.register(IngredientsRecipe, IngredientsRecipeAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
 admin.site.register(ShoppingCart, ShoppingCartAdmin)
