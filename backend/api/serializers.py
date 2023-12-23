d47354eeffd59738b70141eae1cdea6132d486eb
Anton Kabanov <imcreativchik@gmail.com>
2023-12-24 03:04:01 +0500

    refactor for flake8

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -25,8 +25,7 @@ class CustomUserSerializer(UserSerializer):
         if user.is_anonymous:
             return False
         return Subscription.objects.filter(
-            user=user,
-            author=obj.id).exists()
+            user=user, author=obj.id).exists()
 
 
 class CustomUserCreateSerializer(UserCreateSerializer):
