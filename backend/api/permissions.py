74a92cfa0442fffddbb0eaf8b27d73e669923673
Anton Kabanov <imcreativchik@gmail.com>
2023-12-23 21:47:15 +0500

    refactor

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,14 +1,15 @@
 from rest_framework import permissions
-from rest_framework.permissions import SAFE_METHODS
 
 
-class IsUserOrAdmin(permissions.BasePermission):
-    """Админ или авторизованный пользователь."""
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """Автор или только чтение"""
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in SAFE_METHODS
-            or request.user.is_authenticated
-            and request.user.is_active
-            and (request.user == obj.author or request.user.is_staff)
-        )
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return (obj.author == request.user
+                or request.user.is_superuser)
