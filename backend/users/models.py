79941e6e5033b2d90e702b31422ae7a7e5e431cf
Anton Kabanov <imcreativchik@gmail.com>
2023-12-23 21:44:46 +0500

    refactor

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,45 +1,36 @@
-from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-from users.validators import validate_username
+from users.validators import username_validator
 
 
 class User(AbstractUser):
     """Кастомная модель User"""
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = (
-        'username', 'first_name',
-        'last_name', 'password',
+    email = models.EmailField(
+        'Электронная почта',
+        max_length=settings.
+        MAX_LENGHT_EMAIL, unique=True
     )
-
     username = models.CharField(
-        max_length=254,
-        blank=False,
+        verbose_name='Имя пользователя',
         unique=True,
-        validators=[validate_username],
-        verbose_name='Никнейм'
+        max_length=settings.MAX_LENGHT_USERNAME,
+        validators=(username_validator,),
     )
     first_name = models.CharField(
-        max_length=150,
-        blank=False,
-        verbose_name='Имя'
+        'Имя',
+        max_length=settings.
+        MAX_LENGHT_USERNAME,
     )
     last_name = models.CharField(
-        max_length=150,
-        blank=False,
-        verbose_name='Фамилия'
-    )
-    email = models.EmailField(
-        max_length=150,
-        blank=False,
-        unique=True,
-        verbose_name='Электроная почта'
+        'Фамилия',
+        max_length=settings.MAX_LENGHT_USERNAME,
     )
     password = models.CharField(
-        max_length=150,
-        blank=False,
-        verbose_name='Пароль'
+        'Пароль',
+        max_length=settings.MAX_LENGHT_PASSWORD,
     )
 
     class Meta:
@@ -50,24 +41,31 @@ class User(AbstractUser):
         return self.username
 
 
-class Follow(models.Model):
+class Subscription(models.Model):
     """Кастомная модель для подписок на автора"""
+
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        verbose_name='Подписчик',
         related_name='follower',
-        verbose_name='Подписчик'
     )
-    following = models.ForeignKey(
+    author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        verbose_name='Автор рецепта',
         related_name='following',
-        verbose_name='Подписчик автора'
     )
 
     class Meta:
         verbose_name = 'Подписка'
         verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_subscribe'
+            )
+        ]
 
     def __str__(self):
-        return f'{self.user} подписался на {self.following}'
+        return f'{self.user} - {self.author}'
