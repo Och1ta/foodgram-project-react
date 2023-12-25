16483f760b0f1f23a762c4d6adf75551c3df9b13
Anton Kabanov <imcreativchik@gmail.com>
2023-12-25 19:05:57 +0500

    refactor: правки ревьюера

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -3,9 +3,7 @@ from django.contrib import admin
 from users.models import User
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('pk', 'username', 'first_name', 'last_name', 'email')
     search_fields = ('username', 'email',)
-
-
-admin.site.register(User, UserAdmin)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,36 +1,45 @@
-from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import Q, F
 
+from users.constants import (
+    MAX_LENGTH_EMAIL, MAX_LENGTH_USERNAME, MAX_LENGTH_FIRST_NAME,
+    MAX_LENGTH_LAST_NAME, MAX_LENGTH_PASSWORD
+)
 from users.validators import username_validator
 
 
 class User(AbstractUser):
     """Кастомная модель User"""
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = (
+        'username', 'first_name',
+        'last_name', 'password',
+    )
+
     email = models.EmailField(
         'Электронная почта',
-        max_length=settings.
-        MAX_LENGHT_EMAIL, unique=True
+        max_length=MAX_LENGTH_EMAIL,
+        unique=True
     )
     username = models.CharField(
         verbose_name='Имя пользователя',
         unique=True,
-        max_length=settings.MAX_LENGHT_USERNAME,
+        max_length=MAX_LENGTH_USERNAME,
         validators=(username_validator,),
     )
     first_name = models.CharField(
         'Имя',
-        max_length=settings.
-        MAX_LENGHT_USERNAME,
+        max_length=MAX_LENGTH_FIRST_NAME,
     )
     last_name = models.CharField(
         'Фамилия',
-        max_length=settings.MAX_LENGHT_USERNAME,
+        max_length=MAX_LENGTH_LAST_NAME,
     )
     password = models.CharField(
         'Пароль',
-        max_length=settings.MAX_LENGHT_PASSWORD,
+        max_length=MAX_LENGTH_PASSWORD,
     )
 
     class Meta:
@@ -64,7 +73,11 @@ class Subscription(models.Model):
             models.UniqueConstraint(
                 fields=['user', 'author'],
                 name='unique_subscribe'
-            )
+            ),
+            models.CheckConstraint(
+                check=~Q(user=F('author')),
+                name='user_not_author',
+            ),
         ]
 
     def __str__(self):
