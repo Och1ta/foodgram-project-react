d329c1de9267fba6b2b6f6701d696d97df3650d2
Anton Kabanov <imcreativchik@gmail.com>
2023-12-26 20:25:56 +0500

    refactor: правки ревьюера

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    """Автор или только чтение"""
+    """Автор или только чтение."""
 
     def has_object_permission(self, request, view, obj):
         return (
