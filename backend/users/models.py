be0fd0827871c7bb71dd87dc78a54ee29533130a
Anton Kabanov <imcreativchik@gmail.com>
2023-12-28 02:30:04 +0500

    refactor: правки ревьюера

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,16 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator
 from django.db import models
-from django.db.models import Q, F
 
-from users.constants import (
-    MAX_LENGTH_EMAIL, MAX_LENGTH_USERNAME, MAX_LENGTH_FIRST_NAME,
-    MAX_LENGTH_LAST_NAME, MAX_LENGTH_PASSWORD
-)
+from recipes.constants import (MAX_LEN_EMAIL, MAX_LEN_NAME,)
+
 from users.validators import username_validator
 
 
 class User(AbstractUser):
-    """Кастомная модель User"""
+    """Кастомная модель User."""
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = (
@@ -20,65 +19,66 @@ class User(AbstractUser):
 
     email = models.EmailField(
         'Электронная почта',
-        max_length=MAX_LENGTH_EMAIL,
-        unique=True
+        max_length=MAX_LEN_EMAIL,
+        unique=True,
+        validators=[EmailValidator],
     )
     username = models.CharField(
-        verbose_name='Имя пользователя',
+        'Имя пользователя',
+        max_length=MAX_LEN_NAME,
         unique=True,
-        max_length=MAX_LENGTH_USERNAME,
         validators=(username_validator,),
     )
     first_name = models.CharField(
         'Имя',
-        max_length=MAX_LENGTH_FIRST_NAME,
+        max_length=MAX_LEN_NAME,
     )
     last_name = models.CharField(
         'Фамилия',
-        max_length=MAX_LENGTH_LAST_NAME,
-    )
-    password = models.CharField(
-        'Пароль',
-        max_length=MAX_LENGTH_PASSWORD,
+        max_length=MAX_LEN_NAME,
     )
 
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+        ordering = ('username',)
 
     def __str__(self):
-        return self.username
+        return {self.username}, {self.email}
 
 
 class Subscription(models.Model):
-    """Кастомная модель для подписок на автора"""
+    """Кастомная модель для подписок на автора."""
 
     user = models.ForeignKey(
         User,
+        related_name='followed_users',
         on_delete=models.CASCADE,
         verbose_name='Подписчик',
-        related_name='follower',
     )
     author = models.ForeignKey(
         User,
+        related_name='author',
         on_delete=models.CASCADE,
-        verbose_name='Автор рецепта',
-        related_name='following',
+        verbose_name='Автор',
     )
 
     class Meta:
         verbose_name = 'Подписка'
         verbose_name_plural = 'Подписки'
-        constraints = [
+        constraints = (
             models.UniqueConstraint(
-                fields=['user', 'author'],
-                name='unique_subscribe'
+                fields=('user', 'author'),
+                name=(
+                    '\n%(app_label)s_%(class)s пользователь не может '
+                    'дважды подписаться на одного и того же автора\n'),
             ),
-            models.CheckConstraint(
-                check=~Q(user=F('author')),
-                name='user_not_author',
-            ),
-        ]
+        )
 
     def __str__(self):
         return f'{self.user} - {self.author}'
+
+    def save(self, *args, **kwargs):
+        if self.user == self.author:
+            raise ValidationError('Вы не можете подписаться на себя')
+        super().save(*args, **kwargs)
